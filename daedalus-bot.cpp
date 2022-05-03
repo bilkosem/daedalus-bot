30b250d61a9e9f4b5e7644ce0dba2e9f62828ed5
Bilgehan Kösem <bilkos92@gmail.com>
2022-05-03 21:06:12 +0300

    binapi submodule added

---
+++ daedalus-bot.cpp
@@ -0,0 +1,12 @@
+﻿// daedalus-bot.cpp : Defines the entry point for the application.
+//
+
+#include "daedalus-bot.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
